cacb2099f449b7adbd92e4372ed2ead9ef2780d7
王妍 <843668546@qq.com>
2015-12-26 16:16:57 +0800

    twoVC

---
+++ GitTest/GitTest/TwoViewController.h
@@ -0,0 +1,13 @@
+//
+//  TwoViewController.h
+//  GitTest
+//
+//  Created by SCJY on 15/12/26.
+//  Copyright © 2015年 scjy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TwoViewController : UIViewController
+
+@end
---
+++ GitTest/GitTest/TwoViewController.m
@@ -0,0 +1,38 @@
+//
+//  TwoViewController.m
+//  GitTest
+//
+//  Created by SCJY on 15/12/26.
+//  Copyright © 2015年 scjy. All rights reserved.
+//
+
+#import "TwoViewController.h"
+
+@interface TwoViewController ()
+
+@end
+
+@implementation TwoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor cyanColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
