49642bdc9d271428019cc63841e7161713f16c49
张莉 <287081764@qq.com>
2015-12-26 15:31:14 +0800

    新建工程

---
+++ GitTest/GitTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitTest
+//
+//  Created by scjy on 15/12/26.
+//  Copyright (c) 2015年 scjy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
